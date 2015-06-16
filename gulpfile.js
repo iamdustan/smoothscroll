cffe2f9c78dc876fda083da10e69a688b3d2711d
Jere Menichelli <jmenichelli@gmail.com>
2015-06-16 00:44:13 -0300

    removed unnecessary comments

--- gulpfile.js
+++ gulpfile.js
@@ -15,11 +15,8 @@ var paths = {
 
 gulp.task('lint', function() {
   return gulp.src(paths.src)
-    // eslint() attaches the lint output to the eslint property 
-    // of the file object so it can be used by other modules. 
     .pipe(eslint())
-    // eslint.format() outputs the lint results to the console. 
-    // Alternatively use eslint.formatEach() (see Docs). 
+    // outputs the lint results to the console. 
     .pipe(eslint.format())
     // To have the process exit with an error code (1) on 
     // lint error, return the stream and pipe to failOnError last. 
